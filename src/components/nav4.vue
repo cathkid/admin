6c40605ce8f3300b8f94553204c8fd085dd7667d
barry.jiangq <barry.jiang@chinapost-cb.com>
2019-11-08 09:56:33 +0800

    12

--- src/components/nav4.vue
+++ src/components/nav4.vue
@@ -1,22 +1,45 @@
 <template>
   <div align="center">
-  		<h5 class="table-menu">管理信息中心</h5>
-		  <div class="search-box">
-				 <div class="el-col el-col-4">
-
-				 </div>
-			</div>
-			
+  		<h5 class="table-menu">数量统计</h5>
   	  <el-table  v-loading="loading"  :data="tableData"  style="width: 100%;overflow: hidden ">
-		    <el-table-column  prop="countNum" label="生成总条数" > </el-table-column>
 				<el-table-column  prop="countTodayNum" label="今日生成总条数"> </el-table-column>
+				<el-table-column  prop="countpreday" label="昨日生成总条数"> </el-table-column>
+				<el-table-column  prop="countweek" label="本周生成总条数"> </el-table-column>
+				<el-table-column  prop="countmonth" label="本月生成总条数"> </el-table-column>
+				<el-table-column  prop="countNum" label="生成总条数" > </el-table-column>
+		  </el-table>
+		  
+		  <h5 class="table-menu">会员统计</h5>
+		  <el-table  v-loading="loading"  :data="tableData"  style="width: 100%;overflow: hidden ">
+		  	<el-table-column  prop="vipNum" label="VIP会员"> </el-table-column>
+		  	<el-table-column  prop="countmonthvip" label="当月VIP会员"> </el-table-column>
 				<el-table-column  prop="newuser" label="今日活跃用户"> </el-table-column>
 				<el-table-column  prop="countMemberToday" label="今日注册人数" > </el-table-column>
-		    <el-table-column  prop="countMember" label="注册人数" > </el-table-column>
-				<el-table-column  prop="vipNum" label="VIP会员"> </el-table-column>
+		    <el-table-column  prop="countMember" label="总注册人数" > </el-table-column>
+		  </el-table>
+		  
+		  <h5 class="table-menu">收入统计</h5>
+		  <el-table  v-loading="loading"  :data="tableData"  style="width: 100%;overflow: hidden ">
+		  	<el-table-column  prop="countweekmoney" label="会员本周金额"> </el-table-column>
 				<el-table-column  prop="countMonthMoney" label="会员当月金额"> </el-table-column>
+				<el-table-column  prop="moneyCount2018" label="2018年总金额"> </el-table-column>
+				<el-table-column  prop="moneyCount2019" label="2019年总金额"> </el-table-column>
 				<el-table-column  prop="countMoney" label="会员总金额"> </el-table-column>
 		  </el-table>
+		  
+		  <h5 class="table-menu">条数排行</h5>
+		  <el-table  v-loading="loading"  :data="tableData[0].sortnum"  style="width: 100%;overflow: hidden ">
+		  	<el-table-column  prop="username" label="用户名"> </el-table-column>
+				<el-table-column  prop="num" label="总条数"> </el-table-column>
+		  </el-table>
+		  
+		  <h5 class="table-menu">金额排行</h5>
+		  <el-table  v-loading="loading"  :data="tableData[0].sortmoney"  style="width: 100%;overflow: hidden ">
+		  	<el-table-column  prop="username" label="用户名"> </el-table-column>
+				<el-table-column  prop="num" label="总金额"> </el-table-column>
+		  </el-table>
+		  
+		  
 		  
   </div>
 </template>
